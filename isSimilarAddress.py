5891e0b818c392e4d6c89f34239af35297f67c9d
RingofTheKing <guoyuhao4@gmail.com>
2023-02-25 15:58:42 +0800

    tokenize prototype1

--- isSimilarAddress.py
+++ isSimilarAddress.py
@@ -1,11 +1,21 @@
 import re
+import pandas
+
+testString1 = pandas.read_csv("customer_addresses_id.csv").iloc[0,1]
+testString2 = pandas.read_csv("customer_addresses_id.csv").iloc[0,2]
 
 #tokenize
 # by commas
 # sort
 # ignore common words
 
-IGNORED_WORDS = {"ja?l?a?n?", "jakarta"}
-def tokenize(str)
+IGNORED_WORDS = {"ja?l?a?n? ", "jakarta"}
+def tokenize(str):
     strList = str.split(", ")
+    for i in range(len(strList)):
+        for IW in IGNORED_WORDS:
+            strList[i] = re.sub(IW, "", strList[i])
+    strList.sort()
+    return strList
+
 
