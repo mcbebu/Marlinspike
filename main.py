c9739c1e3ba04e39cc1120606a82b35bff4bcbf5
JaredXwos <hatoandaburedo@gmail.com>
2023-02-26 02:19:16 +0800

    Completes the read/write to the database

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from GIS import get_location
 from geopy.geocoders import Nominatim
 from AppUI import app
 from csv import reader, writer
-
+from copy import deepcopy as dc
 temp_data={}
 
 with open('data.txt', 'r', newline='') as f:
@@ -33,7 +33,7 @@ def location_outputs(loc_list, all_list, original_address):
         "longitude": [],
         "description": []
     }
-    temp_all = all_list
+    temp_all = dc(all_list)
     temp_all["address"] = temp_all['address'][::-1]
     for adr in loc_list:
         data_pos = temp_all["address"].index(adr)
@@ -48,11 +48,20 @@ def location_outputs(loc_list, all_list, original_address):
 # print(location_outputs(isSimilarAddress.get_similar_addresses("adr1",2,temp_data,0), temp_data, "adr1"))
 
 def new_data(all_list, new_adr, new_discr):
+    incase = {
+        "address": ["Bukit Batok St 23", "Changi Airport", "Geylang Road", "Jurong East"],
+        "longitude": ["103.75587", "103.989441", "103.8667", "103.741247"],
+        "latitude": ["1.34082", "1.359167", "1.3000", "1.338296"],
+        "description": ["Hungry", "Traveller", "Thirsty", "Worky"]
+    }
     all_list["address"].append(new_adr)
     lat, long = get_location()
     all_list["latitude"].append(lat)
     all_list["longitude"].append(long)
     all_list["description"].append(new_discr)
+    with open('data.txt', 'w', newline='') as f:
+        w = writer(f)
+        [w.writerow(all_list[keys]) for keys in all_list.keys()]
 
 order_address = input_address()
 narrowed_list = isSimilarAddress.get_similar_addresses(order_address, 2,temp_data,0)
