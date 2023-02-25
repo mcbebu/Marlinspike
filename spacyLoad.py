c2543142c2fbfcad0c775a91b174eae66603fb23
RingofTheKing <guoyuhao4@gmail.com>
2023-02-25 21:39:29 +0800

    cool

--- spacyLoad.py
+++ spacyLoad.py
@@ -3,21 +3,22 @@
 # import attrs
 
 import spacy
-nlp = spacy.load('en_core_web_lg')
-tokenizeFirstSample = nlp(u'Walk across the road and turn left after the church')
-tokenizeSecondSample = nlp(u'walk by the church then turn')
+nlp = spacy.load('en_core_web_md')
+tokenizeFirstSample = nlp(u'I slapped him in the face')
+tokenizeSecondSample = nlp(u'he hit me on the head')
 tokenizeThirdSample = nlp(u'walk by the playground and look for a red house')
 
-print(tokenizeFirstSample.similarity(tokenizeSecondSample));
-print(tokenizeThirdSample.similarity(tokenizeFirstSample));
+print(tokenizeFirstSample.similarity(tokenizeSecondSample)); # returns 0.88
+print(tokenizeThirdSample.similarity(tokenizeSecondSample)); # returns 0.77
+# what is a good threshold?
+
 POScounts = tokenizeFirstSample.count_by(spacy.attrs.POS)
 
 for token in tokenizeFirstSample:
-    if token.pos == "NOUN":
-        print(token.text)
+    print(token.text)
 
 # We should remove VERBS such as walk, turn and items that are very common words in most descriptions
 
-# TODO: determine if a description of an area is similar and aggregate them
-# After reaching a locale pointed out by google maps, there are 5 different
+# TODO: determine if a description of an area is similar
+# After reaching a locale pointed out by google maps
 
