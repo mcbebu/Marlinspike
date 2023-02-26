53742c818a6205213a58bcfa409736640bc1f5f8
DougTCH <douglastan037@gmail.com>
2023-02-26 11:04:19 +0800

    Minor edits

--- NinjaGuesser.py
+++ NinjaGuesser.py
@@ -13,6 +13,10 @@ win = Tk()
 # Set the geometry of tkinter frame
 win.geometry("360x740")
 
+def answer_outcome():
+    outcome = Label(win, fg="black", text="Your answer is x km away!", font=('Arial', 20), height=1)
+    outcome.place(relx=0.5, rely=0.9, anchor=CENTER, bordermode=OUTSIDE)
+
 # Create a canvas
 game_answer_placement = Label(win, fg="black", text="Parcel Where", font=('Arial', 30), height=1)
 game_answer_placement.pack()
@@ -37,7 +41,7 @@ game_answer_txt = Entry(win, width=50, borderwidth=2)
 game_answer_txt.insert(0, "Answer here: ")
 game_answer_txt.place(relx=0.5, rely=0.6, height=60, anchor=CENTER, bordermode=OUTSIDE)
 
-game_answer_submit = Button(win, text="Submit", font=60, height=2, width=10)
+game_answer_submit = Button(win, text="Submit", font=60, height=2, width=10, command=answer_outcome)
 
 game_answer_submit.place(relx=0.5, rely=0.7, anchor=CENTER, bordermode=OUTSIDE)
 
